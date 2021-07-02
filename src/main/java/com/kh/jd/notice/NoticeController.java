eb3ed6c3313ec80b75bdc743c6b0df72a95bafd3
holajjam <holajjam@gmail.com>
2021-07-02 18:08:31 +0900

    파일 업로드 다운로드 수정 update to notice/NoticeController.java update to notice/viewNotice.jsp update to mappers/notice/noticeMapper.xml

--- src/main/java/com/kh/jd/notice/NoticeController.java
+++ src/main/java/com/kh/jd/notice/NoticeController.java
@@ -1,31 +1,35 @@
 package com.kh.jd.notice;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.io.OutputStream;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.security.auth.callback.Callback;
+import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,7 +41,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.google.gson.JsonObject;
-import com.kh.jd.work.Work;
 
 @Controller
 public class NoticeController {
@@ -86,7 +89,7 @@ public class NoticeController {
 
 	// 글조회
 	@RequestMapping(value = "/viewNotice", method = RequestMethod.GET)
-	public ModelAndView viewNotice(ModelAndView mv, @RequestParam(name = "n_no") int notice_no)  {
+	public ModelAndView viewNotice(ModelAndView mv, @RequestParam(name = "n_no") int notice_no) {
 		Notice vo = new Notice();
 		vo = noticeService.viewNotice(notice_no);
 		System.out.println("컨트롤러 vo=" + vo);
@@ -103,8 +106,8 @@ public class NoticeController {
 
 	// 글등록
 	@RequestMapping(value = "/addNotice", method = RequestMethod.POST)
-	public ModelAndView addNotice(HttpServletResponse response, HttpServletRequest request
-			,  @RequestParam(name = "notice_filepath", required = false) MultipartFile  multiFile) throws IOException {
+	public ModelAndView addNotice(HttpServletResponse response, HttpServletRequest request,
+			@RequestParam(name = "notice_filepath", required = false) MultipartFile multiFile) throws IOException {
 
 		ModelAndView mv = new ModelAndView();
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -113,31 +116,33 @@ public class NoticeController {
 		String n_sub = "";
 		String n_con = "";
 		int hit = 0;
-		String url = saveFile(multiFile, request);
 		t_no = request.getParameter("teacher_number");
 		m_no = request.getParameter("manager_number");
 		n_sub = request.getParameter("notice_sub");
 		n_con = request.getParameter("notice_con");
-		
+
 		map.put("teacher_number", t_no);
 		map.put("manager_number", m_no);
 		map.put("notice_sub", n_sub);
 		map.put("notice_con", n_con);
 		map.put("hit", hit);
-		// map.put("manager_number", mng); //subject, content, num
 
 		try {
 			if (multiFile != null && !multiFile.equals("")) {
-//				String url = saveFile(multiFile, request);
+//				String path = "resources\\fileUpload\\";   <- 이건 잘못된 방식임
+				String path = request.getServletContext().getRealPath("fileUpload/");
+				File filePath = new File(path); // 만약 fileUpload 폴더가 없다면 폴더생성해줌
+				if(!filePath.exists()) {
+					filePath.mkdirs();
+				}
+				File files = new File(path, multiFile.getOriginalFilename());
+				FileCopyUtils.copy(multiFile.getBytes(), files);
+				map.put("notice_filename", multiFile.getOriginalFilename());
+				map.put("notice_filepath", path);
 			}
-//			String path = "\\resources\\imageUpload\\";
-//			map.put("notice_filepath", path + multiFile.getOriginalFilename());
-			map.put("notice_filename", multiFile.getOriginalFilename());
-			map.put("notice_filepath", url);
-//			map.put("notice_filename", multiFile.getOriginalFilename());
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("이미지 저장에 실패했습니다");
+			System.out.println("파일 저장에 실패했습니다");
 		}
 		try {
 			System.out.println("map : " + map);
@@ -149,106 +154,67 @@ public class NoticeController {
 		return mv;
 	}
 
-	// 파일 저장
-	private String saveFile(MultipartFile report, HttpServletRequest request) {
-		String root = request.getSession().getServletContext().getRealPath("resources");
-		String savePath = root + "\\imageUpload";
-		File folder = new File(savePath);
-		String url = "";
-
-		if (!folder.exists()) {
-			folder.mkdir(); // 폴더가 없다면 생성한다.
-		}
-		String filePath = null;
-		try {
-			System.out.println(report.getOriginalFilename() + "을 저장합니다.");
-			System.out.println("저장 경로 : " + url);
-			filePath = folder + "\\" + report.getOriginalFilename();
-
-			report.transferTo(new File(filePath)); // 파일을 저장한다
-			
-			// cloudinary.uploader().upload(Object file, Map options);
-			  Map config = new HashMap();
-		      config.put("cloud_name", "jdec");
-		      config.put("api_key", "865597575541279");
-		      config.put("api_secret", "SUUC6_OahrI__kqYY808AVXDOco");
-		      Cloudinary cloudinary = new Cloudinary(config);
-		         
-		         Map res = cloudinary.uploader().upload(new File(filePath), ObjectUtils.emptyMap()); 
-		         url = res.get("url") == null ? "" : res.get("url").toString(); 
-		         System.out.println("::::"+url);
-
-			
-			System.out.println("파일 명 : " + report.getOriginalFilename());
-			System.out.println("파일 경로 : " + url);
-			System.out.println("파일 전송이 완료되었습니다.");
-			
-		} catch (Exception e) {
-			System.out.println("파일 전송 에러 : " + e.getMessage());
-		}
-		return url;
-	}
-	
 	// 파일 다운로드
-	@RequestMapping(value = "/fileDownload")
-	public void downloadFile(HttpServletRequest request,HttpServletResponse response) throws Exception {
-		String root = request.getSession().getServletContext().getRealPath("resources");
-//		String savePath = root + "\\imageUpload";
-		String filename = request.getParameter("fileName").substring(44); // 네임넣기
-		System.out.println("filename ="+filename);
-		
-//		String fileroot = request.getParameter()
-//		String realFilename = "";
-//		System.out.println(filename);
-//		
-//		 try {
-//	            String browser = request.getHeader("User-Agent"); 
-//	            //파일 인코딩 
-//	            if (browser.contains("MSIE") || browser.contains("Trident")
-//	                    || browser.contains("Chrome")) {
-//	                filename = URLEncoder.encode(filename, "UTF-8").replaceAll("\\+",
-//	                        "%20");
-//	            } else {
-//	                filename = new String(filename.getBytes("UTF-8"), "ISO-8859-1");
-//	            }
-//	        } catch (UnsupportedEncodingException ex) {
-//	            System.out.println("UnsupportedEncodingException");
-//	        }
-//	        realFilename ="F:\\final\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\JustDo\\resources\\imageUpload"  + filename;
-//	        System.out.println(realFilename);
-//	        File file1 = new File(realFilename);
-//	        if (!file1.exists()) {
-//	            return ;
-//	        }
-//	         
-//	        // 파일명 지정        
-//	        response.setContentType("application/octer-stream");
-//	        response.setHeader("Content-Transfer-Encoding", "binary;");
-//	        response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
-//	        try {
-//	            OutputStream os = response.getOutputStream();
-//	            FileInputStream fis = new FileInputStream(realFilename);
-//	 
-//	            int ncount = 0;
-//	            byte[] bytes = new byte[512];
-//	 
-//	            while ((ncount = fis.read(bytes)) != -1 ) {
-//	                os.write(bytes, 0, ncount);
-//	            }
-//	            fis.close();
-//	            os.close();
-//	        } catch (Exception e) {
-//	            System.out.println("FileNotFoundException : " + e);
-//	        }
-//		byte fileByte[] = org.apache.commons.io.FileUtils.readFileToByteArray(new File(""+filename));
-		
-//		response.setContentType("application/octet-stream");
-//		response.setContentLength(fileByte.length);
-		response.setHeader("Content-Disposition",  "attachment; fileName=http://res.cloudinary.com/jdec/image/upload/"+URLEncoder.encode(filename, "UTF-8"));
-//		response.getOutputStream().write(fileByte);
-		response.getOutputStream().flush();
-		response.getOutputStream().close();
-	    }
+	@RequestMapping(value = "/fileDownload", method = RequestMethod.GET)
+	public void fileDownload(Notice vo, HttpServletRequest req, HttpServletResponse res, @RequestParam(name = "n_no") int notice_no  ) throws Exception {
+
+			vo = noticeService.viewNotice(notice_no);
+			System.out.println("vo = "+vo);
+			
+			HttpSession session 	= req.getSession();
+	    	String filePath 			= vo.getNotice_filepath();//"여기 vo에서 파일패스가져오기"
+	    	String fileName 		= vo.getNotice_filename();
+		 	String fileFullPath 	= filePath + fileName;
+	    	
+			System.out.println("filePath : " + filePath);
+			System.out.println("fileName : " + fileName);
+			System.out.println("fileFullPath : " + fileFullPath);
+		 	File file = new File(fileFullPath);
+		 	System.out.println("file = " + file);
+
+			if(file.exists()) {
+				
+				  String userAgent = req.getHeader("User-Agent");
+				  System.out.println("userAgent = " + userAgent);
+				  boolean ie = userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("rv:11") > -1;
+				  System.out.println("ie = " + ie);
+				  if (ie) {
+					  fileName = URLEncoder.encode((String) fileName, "utf-8");
+				  } else {
+					  fileName = new String(((String) fileName).getBytes("utf-8"),"iso-8859-1");
+				  }
+
+				  res.setContentType("application/octet-stream");
+				  res.setHeader("Content-Disposition","attachment;filename=\"" +fileName+"\";");
+
+				  FileInputStream fis=new FileInputStream(file);
+				  System.out.println("fis = " + fis);
+				  BufferedInputStream bis=new BufferedInputStream(fis);
+				  System.out.println("bis = " + bis);
+				  ServletOutputStream so=res.getOutputStream();
+				  System.out.println("so = " + so);
+				  BufferedOutputStream bos=new BufferedOutputStream(so);
+				  System.out.println("bos = " + bos);
+
+				  byte[] data=new byte[2048];
+				  int input=0;
+				  while((input=bis.read(data))!=-1){
+					  bos.write(data,0,input);
+					  bos.flush();
+				  }
+
+				  if(bos!=null) bos.close();
+				  if(bis!=null) bis.close();
+				  if(so!=null) so.close();
+				  if(fis!=null) fis.close();
+
+			} else {
+				res.setCharacterEncoding("UTF-8"); 
+				res.setContentType("text/html;charset:UTF-8");
+				res.setHeader("Cache-Control", "no-cache");
+				res.getWriter().print("<script>alert('파일이 존재하지 않거나 삭제되었습니다.')</script>");
+			}
+	}
 
 	// 글수정폼
 	@RequestMapping(value = "/editNoticeForm", method = RequestMethod.GET)
@@ -263,7 +229,7 @@ public class NoticeController {
 
 	// 글수정
 	@RequestMapping(value = "/editNotice", method = RequestMethod.POST)
-	public ModelAndView editNotice(HttpServletRequest request, MultipartFile  multiFile) {
+	public ModelAndView editNotice(HttpServletRequest request, MultipartFile multiFile) {
 		ModelAndView mv = new ModelAndView(); // mv 객체 생성
 		Map<String, Object> map = new HashMap<String, Object>();
 		String n_no = "";
@@ -274,28 +240,11 @@ public class NoticeController {
 		n_sub = request.getParameter("n_sub");
 		n_con = request.getParameter("editor1");
 		n_file = request.getParameter("n_file");
-		
+
 		map.put("notice_no", n_no);
 		map.put("notice_sub", n_sub);
 		map.put("notice_con", n_con);
-		
-		try {
-			if (multiFile != null && !multiFile.equals(""))
-				saveFile(multiFile, request);
-			String path = "\\resources\\imageUpload\\";
-			map.put("notice_filepath", path + multiFile.getOriginalFilename());
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("이미지 저장에 실패했습니다");
-		}
-		try {
-			System.out.println("map : " + map);
-			noticeService.addNotice(map);
-			mv.setViewName("redirect:/listNotice");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+
 		System.out.println("컨트롤러 map : " + map);
 		noticeService.editNotice(map);
 		mv.setViewName("redirect:/viewNotice?n_no=" + n_no);
