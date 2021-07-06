09d8221164615103f4e0c31a7a70fd8f44edf599
ghwo515 <01043100515hj@gmail.com>
2021-07-06 22:00:32 +0900

    Update 학습동영상 등록 기능 구현 in Video.java, VideoController.java, VideoDao.java, VideoService.java, VideoServiceImpl.java, videoMapper.xml, listVideo.jsp, videoList.css Insert 학습동영상 등록 화면 구현 in addVideo.jsp

--- src/main/java/com/kh/jd/video/VideoController.java
+++ src/main/java/com/kh/jd/video/VideoController.java
@@ -3,6 +3,9 @@ package com.kh.jd.video;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
@@ -15,27 +18,63 @@ import org.jcodec.scale.AWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.kh.jd.account.Student;
+import com.kh.jd.registration.Registration;
+import com.kh.jd.registration.RegistrationService;
 
 
 @Controller
 public class VideoController {
-	private WebApplicationContext context;
+	@Autowired
+	private RegistrationService RService;
+	@Autowired
+	private VideoService VService;
 	
 	// 학습동영상 리스트
 	@RequestMapping(value = "listVideo", method = RequestMethod.GET)
-	public String listVideo(Model m) {
+	public String listVideo(Model m, HttpServletRequest request) {
+		Student st= (Student)request.getSession().getAttribute("DTO");
+		int student_number=st.getStudent_number();
+		List<Registration> list = RService.listRegistration(student_number);
+		m.addAttribute("list", list);
 		return "/video/listVideo";
 	}
 	
-	// 동영상 업로드 및 썸네일 추출
+	// 등록 폼
+	@RequestMapping(value = "addVideo", method = RequestMethod.GET)
+	public String addVideo(Model m,HttpServletRequest request) {
+		String number = request.getParameter("lectureclass_no");
+		m.addAttribute("number", number);
+		return "video/addVideo";
+	}
+	
+	// 학습동영상 썸네일 출력
+	@ResponseBody
+	@RequestMapping(value = "listThumbnail", method = RequestMethod.POST)
+	public String listThumbnail(HttpServletRequest request, @RequestParam(name="lectureclass_no") int lectureclass_no) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("list", VService.listVideo(lectureclass_no));
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String jsonOutput = gson.toJson(map);
+		return jsonOutput;
+	}
+	
+	// 학습동영상 등록 및 영상 업로드, 썸네일 추출
 	@RequestMapping(value = "addVideo", method = RequestMethod.POST)
-	public String addVideo(Model m, @RequestParam("file") MultipartFile multipartFile
-			,HttpServletRequest request) {
+	public String addVideo(Model m, @RequestParam(name="video_file_org") MultipartFile multipartFile
+			,HttpServletRequest request, Video video) {
 		String path = request.getServletContext().getRealPath("videoUpload/");
 		File filePath = new File(path);
 		
@@ -46,18 +85,24 @@ public class VideoController {
 		File targetFile = new File(path + multipartFile.getOriginalFilename());  // TODO
 		String name = multipartFile.getOriginalFilename(); // 동영상 파일 이름
 		String fullName = path + multipartFile.getOriginalFilename(); // 경로 + 동영상 파일 이름
+		String imgName ="";
 		try {
 			InputStream fileStream = multipartFile.getInputStream();
 			FileUtils.copyInputStreamToFile(fileStream, targetFile);
 			System.out.println("파일 업로드 성공~~~~~~~~~~~~~~~~~~~");
-			String imgName = getMovieThumbnail(fullName, name); // 추출한 썸네일 이름
+			imgName = getMovieThumbnail(fullName, name); // 추출한 썸네일 이름
 			System.out.println(imgName);
 		} catch (Exception e) {
 			FileUtils.deleteQuietly(targetFile);
 			e.printStackTrace();
 			System.out.println("파일 업로드 실패~~~~~~~~~~~~~~~~~~~");
 		}
-		return ""; //TODO 리스트로 이동할 예정
+		video.setVideo_file(name);
+		video.setVideo_path(path);
+		video.setVideo_image(imgName);
+		System.out.println(video);
+		VService.addVideo(video);
+		return "redirect:/listVideo";
 	}
 	
 	// 썸네일 추출 함수
--- src/main/webapp/WEB-INF/views/video/listVideo.jsp
+++ src/main/webapp/WEB-INF/views/video/listVideo.jsp
@@ -9,12 +9,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>JD 교육원</title>
 <link rel="stylesheet" href="resources/css/bootstrap/bootstrap.css">
-<link rel="stylesheet" href="<c:url value="/resources/css/fonts.css"/>">
-<link rel="stylesheet" href="<c:url value="/resources/css/common/header.css"/>">
+<link rel="stylesheet" href="resources/css/fonts.css">
+<link rel="stylesheet" href="resources/css/common/header.css">
 <link rel="stylesheet" href="resources/css/videoList.css"/>
-<link rel="stylesheet" href="<c:url value="/resources/css/common/footer.css"/>">
+<link rel="stylesheet" href="resources/css/common/footer.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="<c:url value="/resources/js/header.js"/>"></script>
+<script type="text/javascript" src="resources/js/header.js"></script>
 </head>
 <body>
 	<div class="wrapper"><jsp:include page="../common/header.jsp"></jsp:include></div>
@@ -22,23 +22,53 @@
 			<div>
 				<p class="ln_title">학습동영상</p>
 				<p>수강과목</p>
-				<select>
-					<option>ㄱㄱㄱㄱ</option>
+				<select name="title" onchange="goTitle()" id="listTitle">
+					<option>강의목록</option>
+					<c:forEach var="vo" items="${list}">
+					<option value="${vo.lectureclass_no}">${vo.lecture_title} Class:${vo.lectureclass_class}</option>
+					</c:forEach>
 				</select>
-				<p>과목과목과목</p>
-				<progress></progress>
-				<progress></progress>
+				<p id="lecTitle"></p>
+				전체 평균 진도율 : <progress></progress>
+				강의 평균 진도율 : <progress></progress>
+				<a href="#" onclick="goAdd()">등록</a>
 			</div>
-			<form action="addVideo" method="post" enctype="multipart/form-data">
-				<input type="file" value="파일선택" name="file">
-				<input type="submit" value="업로드">
-			</form>
 			<div>
-				<ul>
-					<li><a></a></li>
+				<ul class="thum">
+				
 				</ul>
 			</div>
 	</div>
 	<jsp:include page="../common/footer.jsp"></jsp:include>
+	<script>
+		// 셀릭트 선택 시 과목 출력
+		function goTitle(){
+			var opt = document.getElementById("listTitle");
+			var optText = opt.options[opt.selectedIndex].text;
+			var optVal = opt.options[opt.selectedIndex].value; // lectureclass 번호 값
+			$("#lecTitle").text(optText);
+			$.ajax({
+				url:"listThumbnail",
+				type:"POST",
+				data:{
+					lectureclass_no : optVal
+				},
+				dataType:"json",
+				success:function(data){
+					var list = "";
+					
+				},
+				error:function(){
+					
+				}
+			});
+		}
+		// 등록 폼으로 이동
+		function goAdd(){
+			var opt = document.getElementById("listTitle");
+			var optVal = opt.options[opt.selectedIndex].value; // lectureclass 번호 값
+			location.href="addVideo?lectureclass_no="+optVal;
+		}
+	</script>
 </body>
 </html>
